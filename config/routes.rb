f8ac16a275d430b36fd689b2afc766252c754e3f
Ulas <uli.will.code@protonmail.com>
2020-08-25 16:54:31 +0300

    Adds show action to the users controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
-
+      resources :users, only: [:show]
     end
   end
 end
