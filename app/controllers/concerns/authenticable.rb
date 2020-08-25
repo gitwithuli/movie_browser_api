1964e8e8dd5748decc8a39d963435327c8a5a56d
Ulas <uli.will.code@protonmail.com>
2020-08-25 17:54:04 +0300

    Adds authenticable module for managing authentication methods

---
+++ app/controllers/concerns/authenticable.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+# This shiny device polishes bared foos
+module Authenticable
+  def current_user
+    return @current_user if @current_user
+
+    header = request.headers['Authorization']
+
+    return nil if header.nil?
+
+    decoded = JsonWebToken.decode(header)
+    @current_user = User.find(decoded[:user_id]) rescue ActiveRecord::RecordNotFound
+  end
+end
