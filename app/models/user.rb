fa368b1c5c022eb3e240102386d549d0c9bb2915
Ulas <uli.will.code@protonmail.com>
2020-08-25 17:23:28 +0300

    Adds update action the users controller

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 
+# This shiny device polishes bared foos
 class User < ApplicationRecord
   validates :email, uniqueness: true
   validates_format_of :email, with: /@/
